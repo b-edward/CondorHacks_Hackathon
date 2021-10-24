5408c2eed72f438ec6211a909e87b84a5ed94dbc
Edward Boado <Eboado8862@conestogac.on.ca>
2021-10-24 11:12:23 -0400

    initial commit

---
+++ FoodSaver/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FoodSaver.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FoodSaver.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}

a624067d1f5bc2266c5a75138bffbc512df6671c
Edward Boado <Eboado8862@conestogac.on.ca>
2021-10-27 16:07:36 -0400

    new solution without iOS

---
+++ FoodSaverApp/FoodSaverApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FoodSaverApp.ViewModels;
+using FoodSaverApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FoodSaverApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}

2adc308012710ca1d6dcf0869fc7e75d9d3bb0cf
Gavin Abeele <abeele.s.gavin@gmail.com>
2021-10-29 13:29:28 -0400

    Added a logout functionaity. and a new tab option called settings to hole logout button.

---
+++ FoodSaver/Views/SettingsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodSaver.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Settings : ContentPage
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private  void LogoutButton_Clicked(object sender, EventArgs e)
+        {
+            //Go to login page 
+            
+            Application.Current.MainPage = (new LoginPage());
+        }
+       
+    }
+}
